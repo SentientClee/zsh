50d0d4f35de1a06b06b11e2cb32881676b24ea6c
SentientClee <clee@goguardian.com>
2026-04-16 20:39:11 -0700

    Bring .. functionality back

--- aliases.zsh
+++ aliases.zsh
@@ -1,5 +1,20 @@
 alias vim="nvim"
 
+# cd into a bare path (enables the dot-expansion widget below)
+setopt AUTO_CD
+
+# Typing `...` expands to `../..`, `....` to `../../..`, etc.
+rationalise-dot() {
+  if [[ $LBUFFER = *.. ]]; then
+    LBUFFER+=/..
+  else
+    LBUFFER+=.
+  fi
+}
+zle -N rationalise-dot
+bindkey . rationalise-dot
+bindkey -M isearch . self-insert
+
 # git
 alias ga='git add'
 alias gc='git commit'
